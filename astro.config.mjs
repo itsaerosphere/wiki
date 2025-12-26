c8646787fa9253441b1051e2f1d13d6982dd0625
Julien Ducharme <julien.ducharme.z@gmail.com>
2025-12-26 16:46:32 -0500

    Added config for title and fixed formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,62 +15,63 @@ export default defineConfig({
     trailingSlash: "ignore",
     integrations: [
         starlight({
-            title: 'Wiki',
-			logo: {
-				light: './src/assets/logo-light.svg',
-				dark: './src/assets/logo-dark.svg',
+            title: 'Spherico Wiki',
+		    logo: {
+			light: './src/assets/logo-light.svg',
+			dark: './src/assets/logo-dark.svg',
+			replacesTitle: true,
+		    },
+		    favicon: '/favicon.ico',
+		    lastUpdated: false,
+		    plugins: [
+			ion({
+			    footer: {
+				text: 'Built with spite, weaponized autism and plenty of swearing.',
+			    },
+			    icons: undefined
+			}),
+		    ],
+		    tableOfContents: true,
+		    social: [
+			{ icon: 'github', label: 'Source', href: 'https://github.com/itsaerosphere/wiki' },
+			{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/dSjmeM5yAy', },
+		    ],
+		    sidebar: [
+			{
+			    label: '[home] Home',
+			    link: '/',
 			},
-			favicon: '/favicon.ico',
-			lastUpdated: false,
-			plugins: [
-				ion({
-					footer: {
-						text: 'Built with spite, weaponized autism and plenty of swearing.',
-					},
-					icons: undefined
-				}),
-			],
-			tableOfContents: true,
-			social: [
-				{ icon: 'github', label: 'Source', href: 'https://github.com/itsaerosphere/wiki' },
-				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/dSjmeM5yAy', },
-			],
-			sidebar: [
-				{
-					label: '[home] Home',
-					link: '/',
-				},
-				{
-					label: 'Rules',
-					link: '/rules',
-				},
-				{
-					label: 'Minecraft S4',
-					autogenerate: {
-						directory: 'minecraft/s4', 
-						collapsed: false,
-					},
-					badge: {
-						text: 'Active',
-						variant: 'success',
-					},
-				},
-				{
-					label: 'Minecraft S5',
-					autogenerate: {
-						directory: '', 
-						collapsed: true,
-					},
-					badge: {
-						text: 'Coming soon!',
-						variant: 'caution',
-					},
-				},
-				{
-					label: 'World Map',
-					link: 'https://map.spherico.one',
-				},
-			],
+			{
+			    label: 'Rules',
+			    link: '/rules',
+			},
+			{
+			    label: 'Minecraft S4',
+			    autogenerate: {
+				    directory: 'minecraft/s4', 
+				    collapsed: false,
+			},
+			    badge: {
+				    text: 'Active',
+				    variant: 'success',
+			    },
+			},
+			{
+			    label: 'Minecraft S5',
+			    autogenerate: {
+				    directory: '', 
+				    collapsed: true,
+			    },
+			    badge: {
+				    text: 'Coming soon!',
+				    variant: 'caution',
+			    },
+			},
+			{
+			    label: 'World Map',
+			    link: 'https://map.spherico.one',
+			},
+		    ],
         }),
     ],
 });
